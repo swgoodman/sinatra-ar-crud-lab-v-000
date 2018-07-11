0461f0d6ae57758573a4dee3531228e05ba05004
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-11 16:42:13 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,9 @@
 
 class Post < ActiveRecord::Base
   attr_accessor :name, :content
-  @@posts = []
 
   def initialize(params)
     @name = params[:name]
     @content = params[:content]
-
-    @@posts << self
   end
 end
