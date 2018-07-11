bf7a462977ecf84f17d9a3546ae5c4368dbb73a5
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-11 16:42:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
+  attr_accessor :post_name, :post_content
 
   def initialize(params)
     @name = params[:name]
